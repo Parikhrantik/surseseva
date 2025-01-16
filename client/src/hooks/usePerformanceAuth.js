1bfa4d11e004f4986061f6c25baa2cdaaa316a8a
Web Expert <36933789+360webexpert@users.noreply.github.com>
2025-01-16 16:08:22 +0530

    Update usePerformanceAuth.js

--- client/src/hooks/usePerformanceAuth.js
+++ client/src/hooks/usePerformanceAuth.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-const API_URL = process.env.BASE_URL || 'http://localhost:5000';
+const API_URL = process.env.BASE_URL || 'http://35.208.79.246/node';
 
 const usePerformanceAuth = (id) => {
   const [isLoading, setIsLoading] = useState(false);
