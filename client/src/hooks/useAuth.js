c876d1cf816ac1c0a3ea31d50aec3fe747ddd03b
Web Expert <36933789+360webexpert@users.noreply.github.com>
2025-01-16 16:07:18 +0530

    Update useAuth.js

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = process.env.BASE_URL || 'http://localhost:5000';
+const API_URL = process.env.BASE_URL || 'http://35.208.79.246/node';
 
 const useAuth = () => {
   const [isLoading, setIsLoading] = useState(false);
